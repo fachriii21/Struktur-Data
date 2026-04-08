5628e61a66aa7bb9a0a6a3fdf3a809ca5b67fd73
fachriii21 <fachrialfaruq40@gmail.com>
2026-04-08 16:15:19 +0800

    Add files via upload

---
+++ Muhammad_Fachri_Al_Faruq_Pencarian.py
@@ -0,0 +1,16 @@
+nama = "Muhammad Fachri Al Faruq"
+target = "a"
+
+
+posisi = -1
+
+for i in range(len(nama)):
+    if nama[i] == target:
+        posisi = i
+        break  
+
+if posisi != -1:
+    print(f"Huruf '{target}' pertama ditemukan pada indeks ke-{posisi}")
+
+else:
+    print("Huruf tidak ditemukan.")
